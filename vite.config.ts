2715fe7d6a23c06a01168d07f710a451d0ee9a9b
opuopu <nurmdopu428@gmail.com>
2024-08-24 00:10:48 +0600

    Fix:Fixed

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "159.223.184.53", // Set the IP address
-    port: 6000, // Set the port
+    port: 5183, // Set the port
   },
 });
